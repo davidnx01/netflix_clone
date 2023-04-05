c6ac4ae8823f511998eccee819204f0ad8078f75
davidnx01 <info@webdavid.sk>
2023-04-05 15:06:13 +0200

    Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -24,11 +25,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="text-2xl text-green-500">dsds</div>
-      <p className="text-white">Logged in as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <Navbar />
     </>
   )
 }
